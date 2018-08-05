a2c9d7f0db14d04e482f221726c151746f84e076
PetarIvancevic <petar.ivancevic171@gmail.com>
2018-08-05 12:23:54 +0200

    updated schema

--- schema.sql
+++ schema.sql
@@ -2,31 +2,39 @@ DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
 
 CREATE TABLE "user" (
-  id VARCHAR(254) NOT NULL,
-  name VARCHAR(254) NOT NULL,
-  email VARCHAR(254) NOT NULL,
-  CONSTRAINT user_pk PRIMARY KEY (id),
-  CONSTRAINT email_unique UNIQUE (email)
+  id VARCHAR(255) NOT NULL,
+  email TEXT NOT NULL,
+  name TEXT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (email)
+);
+
+CREATE TABLE category (
+  id SERIAL,
+  name TEXT NOT NULL,
+  user_id VARCHAR(255) NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+  PRIMARY KEY (id),
+  UNIQUE (user_id, name)
 );
 
 CREATE TABLE wallet (
   id SERIAL,
-  currency SMALLINT NOT NULL,
-  paycheck_day SMALLINT NOT NULL,
   amount INTEGER NOT NULL DEFAULT 0,
-  user_id VARCHAR(254) NOT NULL,
-  CONSTRAINT wallet_pk PRIMARY KEY (id),
-  CONSTRAINT wallet_user_fk FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE,
-  CONSTRAINT paycheck_day_chk CHECK (paycheck_day BETWEEN 1 AND 30)
+  currency SMALLINT NOT NULL,
+  name TEXT NOT NULL,
+  paycheck_day SMALLINT NOT NULL,
+  user_id VARCHAR(255) NOT NULL REFERENCES "user" (id) ON DELETE CASCADE,
+  PRIMARY KEY (id),
+  CONSTRAINT paycheck_day_check CHECK (paycheck_day BETWEEN 1 AND 31)
 );
 
-CREATE TABLE expense (
+CREATE TABLE "transaction" (
   id SERIAL,
-  category SMALLINT NOT NULL,
   amount INTEGER NOT NULL,
-  date DATE NOT NULL,
-  place VARCHAR(60) NOT NULL,
-  wallet_id INTEGER NOT NULL,
-  CONSTRAINT expense_pk PRIMARY KEY (id),
-  CONSTRAINT expense_wallet_fk FOREIGN KEY (wallet_id) REFERENCES wallet (id) ON DELETE CASCADE
+  category_id INTEGER NOT NULL REFERENCES category (id) ON DELETE RESTRICT,
+  comment TEXT,
+  "date" DATE NOT NULL,
+  place TEXT NOT NULL,
+  wallet_id INTEGER NOT NULL REFERENCES wallet (id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
 );
