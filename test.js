ecfaf6744ec861d78ac79fdfbfa93be2275251ff
PetarIvancevic <petar.ivancevic171@gmail.com>
2018-08-05 12:35:55 +0200

    updated tests with user

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 const _ = require('lodash')
 const assert = require('assert')
+const jwt = require('jsonwebtoken')
 const supertest = require('supertest')
 const tape = require('tape')
 
-const {pgp} = require('db')
+const {db, pgp} = require('db')
 const request = supertest(require('server').callback())
 
-const cache = new Map()
-const store = new Map()
-
-tape.onFinish(async function () {
-  await pgp.end()
+tape.onFinish(function () {
+  pgp.end()
+  process.exit(0)
 })
 
 process.on('unhandledRejection', function (reason) {
@@ -18,9 +17,29 @@ process.on('unhandledRejection', function (reason) {
   process.exit(1)
 })
 
+function truncateTable (table) {
+  return db.none(`TRUNCATE $[table~] RESTART IDENTITY CASCADE`, {table})
+}
+
+async function truncateDatabase () {
+  const tables = _(await db.many(`
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = $[tableSchema]
+  `, {tableSchema: 'public'}))
+  .filter(function (table) {
+    return table.table_name !== 'SequelizeMeta'
+  })
+  .map('table_name')
+  .value()
+
+  return Promise.map(tables, truncateTable)
+}
+
 function test () {
   const cb = _.last(arguments)
   tape(..._.initial(arguments), async function (t) {
+    await truncateDatabase()
     await cb(t)
     t.end()
   })
@@ -29,7 +48,8 @@ function test () {
 function testOnly () {
   const cb = _.last(arguments)
   tape.only(..._.initial(arguments), async function (t) {
-    await cb(t, store)
+    await truncateDatabase()
+    await cb(t)
     t.end()
   })
 }
@@ -37,7 +57,8 @@ function testOnly () {
 function api () {
   const cb = _.last(arguments)
   tape(..._.initial(arguments), async function (t) {
-    await cb(t, request, store)
+    await truncateDatabase()
+    await cb(t, request)
     t.end()
   })
 }
@@ -45,26 +66,38 @@ function api () {
 function apiOnly () {
   const cb = _.last(arguments)
   tape.only(..._.initial(arguments), async function (t) {
-    await cb(t, request, store)
+    await truncateDatabase()
+    await cb(t, request)
     t.end()
   })
 }
 
-async function auth (email) {
-  if (!cache.has(email)) {
-    const r = await request.post('/auth').send({email})
-    const token = _.get(r, 'body.data.token')
-    assert(token, 'error getting token in test auth helper')
-    cache.set(email, token)
-  }
+const testUser = {
+  id: 'testuserid',
+  email: 'testuseremail@mail.com',
+  name: 'test user',
+}
+
+async function createUser (email = testUser.email) {
+  return db.one(`
+    INSERT INTO "user" (id, email, name)
+    VALUES ($[id], $[email], $[name])
+    RETURNING id
+  `, {...testUser, email})
+}
+
+function auth (id) {
+  const token = jwt.sign({id}, process.env.JWT_SECRET)
   return {
-    'Authorization': `Bearer ${cache.get(email)}`,
+    'Authorization': `Bearer ${token}`
   }
 }
 
 test.auth = auth
 test.api = api
 test.api.skip = tape.skip
+test.createUser = createUser
+test.user = testUser
 test.skip = tape.skip
 test.skip.api = tape.skip
 test.only = testOnly
